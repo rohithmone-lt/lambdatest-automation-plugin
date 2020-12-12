4e16ead3a373f48877740a197a3e52d367dc63f0
Divyank Singh <divyanks@lambdatest.com>
2020-12-12 18:32:41 +0530

    adding reporter plugin in Jenkins

--- src/main/java/com/lambdatest/jenkins/freestyle/AbstractFreeStyleBuildAction.java
+++ src/main/java/com/lambdatest/jenkins/freestyle/AbstractFreeStyleBuildAction.java
@@ -1,56 +1,62 @@
-package com.lambdatest.jenkins.freestyle;
+/*
+Removed Iframe in new plugin version, uncomment this to implement it again
+*/
 
-import hudson.model.Action;
-import hudson.model.Run;
 
-public abstract class AbstractFreeStyleBuildAction implements Action {
-	private Run<?, ?> build;
 
-	protected String displayName;
-	protected String iconFileName;
-	protected String testUrl;
-
-	@Override
-	public String getIconFileName() {
-		return iconFileName;
-	}
-
-	@Override
-	public String getDisplayName() {
-		return displayName;
-	}
-
-	@Override
-	public String getUrlName() {
-		return testUrl;
-	}
-
-	public Run<?, ?> getBuild() {
-		return build;
-	}
-
-	public void setBuild(Run<?, ?> build) {
-		this.build = build;
-	}
-
-	public void setIconFileName(String iconFileName) {
-		this.iconFileName = iconFileName;
-	}
-
-	public void setDisplayName(String dn) {
-		// the displayname does not wrap after 46 characters
-		// so it will bleed into the view
-		int maxCharactersViewable = 46;
-		if (dn.length() > maxCharactersViewable - 3) {
-			// going to cut the string down and add "..."
-			dn = dn.substring(0, maxCharactersViewable - 3);
-			dn += "...";
-		}
-		displayName = dn;
-	}
-
-	public void setTestUrl(String testUrl) {
-		this.testUrl = testUrl.replaceAll("[:.()|/ ]", "").toLowerCase();
-	}
-
-}
+//package com.lambdatest.jenkins.freestyle;
+//
+//import hudson.model.Action;
+//import hudson.model.Run;
+//
+//public abstract class AbstractFreeStyleBuildAction implements Action {
+//	private Run<?, ?> build;
+//
+//	protected String displayName;
+//	protected String iconFileName;
+//	protected String testUrl;
+//
+//	@Override
+//	public String getIconFileName() {
+//		return iconFileName;
+//	}
+//
+//	@Override
+//	public String getDisplayName() {
+//		return displayName;
+//	}
+//
+//	@Override
+//	public String getUrlName() {
+//		return testUrl;
+//	}
+//
+//	public Run<?, ?> getBuild() {
+//		return build;
+//	}
+//
+//	public void setBuild(Run<?, ?> build) {
+//		this.build = build;
+//	}
+//
+//	public void setIconFileName(String iconFileName) {
+//		this.iconFileName = iconFileName;
+//	}
+//
+//	public void setDisplayName(String dn) {
+//		// the displayname does not wrap after 46 characters
+//		// so it will bleed into the view
+//		int maxCharactersViewable = 46;
+//		if (dn.length() > maxCharactersViewable - 3) {
+//			// going to cut the string down and add "..."
+//			dn = dn.substring(0, maxCharactersViewable - 3);
+//			dn += "...";
+//		}
+//		displayName = dn;
+//	}
+//
+//	public void setTestUrl(String testUrl) {
+//		this.testUrl = testUrl.replaceAll("[:.()|/ ]", "").toLowerCase();
+//	}
+//
+//}
---
+++ src/main/java/com/lambdatest/jenkins/freestyle/Report/ReportBuildAction.java
@@ -0,0 +1,139 @@
+package com.lambdatest.jenkins.freestyle.Report;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import hudson.model.Run;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.lambdatest.jenkins.freestyle.api.Constant;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.*;
+import java.util.logging.Logger;
+
+public class ReportBuildAction extends AbstractReportBuildAction {
+    private final static Logger logger = Logger.getLogger(ReportBuildAction.class.getName());
+    private String lambdaTestBuildBrowserUrl;
+    private String authString;
+    private String buildName;
+    List<JSONObject> result = new ArrayList<JSONObject>();
+
+    public ReportBuildAction(final Run<?, ?> build, String name,String password, String buildName) {
+        super();
+        setBuild(build);
+        this.authString =  name + ":" + password;
+        this.buildName = buildName;
+    }
+    public void generateLambdaTestReport() {
+        logger.info("authString : " + authString);
+        byte[] authEncBytes = Base64.getEncoder().encode(authString.getBytes());
+        String authStringEnc = new String(authEncBytes);
+        logger.info("in generate generateLambdaTestReport function");
+        try {
+            URL buildUrl = new URL(Constant.Report.BUILD_INFO_URL);
+            URLConnection buildUrlConnection = buildUrl.openConnection();
+            buildUrlConnection.setRequestProperty("Authorization", "Basic " + authStringEnc);
+            InputStream is = buildUrlConnection.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            int numCharsReadForBuild;
+            char[] charArrayForBuild = new char[1024];
+            StringBuffer sb = new StringBuffer();
+            while ((numCharsReadForBuild = isr.read(charArrayForBuild)) > 0) {
+                sb.append(charArrayForBuild, 0, numCharsReadForBuild);
+            }
+            String buildResult = sb.toString();
+            try {
+                ObjectMapper mapper = new ObjectMapper();
+                JsonNode rootNode = mapper.readTree(buildResult);
+                ArrayNode dataNode = (ArrayNode) rootNode.get("data");
+                Iterator<JsonNode> dataIterator = dataNode.elements();
+                while (dataIterator.hasNext()) {
+                    JsonNode buildDetailNode = dataIterator.next();
+                    if (buildDetailNode.get("name").toString().replaceAll("\"", "").equals(buildName)) {
+                        String build_id = buildDetailNode.get("build_id").toString();
+
+                        lambdaTestBuildBrowserUrl = "https://automation.lambdatest.com/logs/?build=" + build_id;
+
+                        logger.info("lambdaTestBuildBrowserUrl : " + lambdaTestBuildBrowserUrl);
+                        URL sessionUrl = new URL(Constant.Report.SESSION_INFO_URL + "?build_id=" + build_id);
+                        URLConnection sessionUrlConnection = sessionUrl.openConnection();
+                        sessionUrlConnection.setRequestProperty("Authorization", "Basic " + authStringEnc);
+                        is = sessionUrlConnection.getInputStream();
+                        isr = new InputStreamReader(is);
+                        int numCharsReadForSession;
+                        char[] charArrayForSession = new char[1024];
+                        sb = new StringBuffer();
+                        while ((numCharsReadForSession = isr.read(charArrayForSession)) > 0) {
+                            sb.append(charArrayForSession, 0, numCharsReadForSession);
+                        }
+                        String sessionResult = sb.toString();
+
+                        JsonNode sessionRootNode = mapper.readTree(sessionResult);
+                        ArrayNode sessionDataNode = (ArrayNode) sessionRootNode.get("data");
+                        Iterator<JsonNode> sessionDataIterator = sessionDataNode.elements();
+
+                        JSONArray array = new JSONArray();
+                        while (sessionDataIterator.hasNext()) {
+                            JsonNode sessionDetailNode = sessionDataIterator.next();
+                            String testUrl = "https://automation.lambdatest.com/logs/?testID=" + sessionDetailNode.get("test_id").toString().replaceAll("\"", "") + "&page=1&build=" + build_id;
+                            logger.info("testUrl : " + testUrl);
+                            JSONObject resultJSON = new JSONObject();
+                            resultJSON.put("url",testUrl);
+                            resultJSON.put("status",sessionDetailNode.get("status_ind").toString().replaceAll("\"", ""));
+                            if(sessionDetailNode.has("device")) {
+                                resultJSON.put("browser",sessionDetailNode.get("device").toString().replaceAll("\"", ""));
+                            } else {
+                                resultJSON.put("browser",sessionDetailNode.get("browser").toString().replaceAll("\"", ""));
+                            }
+                            resultJSON.put("browserVersion",sessionDetailNode.get("version").toString().replaceAll("\"", ""));
+                            resultJSON.put("OS",sessionDetailNode.get("platform").toString().replaceAll("\"", ""));
+                            resultJSON.put("name",sessionDetailNode.get("name").toString().replaceAll("\"", ""));
+                            resultJSON.put("testId",sessionDetailNode.get("test_id").toString().replaceAll("\"", ""));
+                            resultJSON.put("testDuration",sessionDetailNode.get("duration").toString().replaceAll("\"", ""));
+                            array.put(resultJSON);
+                        }
+                        for (int i = 0; i < array.length(); i++) {
+                            result.add(array.getJSONObject(i));
+                        }
+                        logger.info(result.toString());
+                        break;
+                    }
+                }
+            } catch (JsonParseException e) {
+                e.printStackTrace();
+            } catch (JsonMappingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<JSONObject> getResult() {
+        return result;
+    }
+
+    public String getLambdaTestBuildBrowserUrl() {
+        return lambdaTestBuildBrowserUrl;
+    }
+
+    public String getBuildName() {
+        return buildName;
+    }
+}
